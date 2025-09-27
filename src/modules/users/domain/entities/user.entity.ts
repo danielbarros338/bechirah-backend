4f8d4fcd44dd94c3f1df3705809acb5c9a8c2506
Daniel <usrrufus12@omint.com.br>
2025-09-27 17:18:47 -0300

    implementada entidade de domínio

--- src/modules/users/domain/entities/user.entity.ts
+++ src/modules/users/domain/entities/user.entity.ts
@@ -1,11 +1,19 @@
 import { CryptUtils } from '../../../../shared/crypt';
 import { Email } from '../value-objects/email.vo';
+import { IsActive } from '../value-objects/isActive.vo';
+import { Name } from '../value-objects/name.vo';
 import { Password } from '../value-objects/password.vo';
+import { Role } from '../value-objects/role.vo';
+import { Username } from '../value-objects/username.vo';
 
 export class User {
   constructor(
+    private name: Name,
+    private username: Username,
     private readonly email: Email,
     private password: Password,
+    private isActive: IsActive,
+    private role: Role,
   ) {}
 
   verifyPassword(plain: string, crypt: CryptUtils): boolean {
@@ -18,5 +26,51 @@ export class User {
 
   get emailValue(): string {
     return this.email.value;
+  }
+
+  get usernameValue(): string {
+    return this.username.value;
+  }
+
+  get nameValue(): string {
+    return this.name.value;
+  }
+
+  get isActiveValue(): boolean {
+    return this.isActive.value;
+  }
+
+  get roleValue(): string {
+    return this.role.value;
+  }
+
+  activate(): void {
+    this.isActive = IsActive.create(true);
+  }
+
+  deactivate(): void {
+    this.isActive = IsActive.create(false);
+  }
+
+  changeRole(newRole: string): void {
+    this.role = Role.create(newRole);
+  }
+
+  static create(
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    isActive: boolean,
+    role: string,
+  ): User {
+    return new User(
+      Name.create(name),
+      Username.create(username),
+      Email.create(email),
+      Password.fromPlain(password, CryptUtils.prototype),
+      IsActive.create(isActive),
+      Role.create(role),
+    );
   }
 }
